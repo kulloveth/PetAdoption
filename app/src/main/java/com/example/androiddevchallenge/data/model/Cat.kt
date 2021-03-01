0ed83102f4e49c605a0f41c2e45a336b6a1b120b
kulloveth <kulloveth@gmail.com>
2021-03-01 20:27:07 +0100

    format model

--- app/src/main/java/com/example/androiddevchallenge/data/model/Cat.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/model/Cat.kt
@@ -1,13 +1,13 @@
 package com.example.androiddevchallenge.data.model
 
 data class Cat(
-    val id:Int = 0 ,
-    val name:String = "",
-    val desc:String = "",
-    val breed:String = "",
-    val imgUrl:String = "",
-    val location:String = "",
-    val color:String = "",
-    val fee:String = "",
+    val id: Int = 0,
+    val name: String = "",
+    val desc: String = "",
+    val breed: String = "",
+    val imgUrl: String = "",
+    val location: String = "",
+    val color: String = "",
+    val fee: String = "",
     val owner: Owner = Owner()
 )
--- app/src/main/java/com/example/androiddevchallenge/data/model/Owner.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/model/Owner.kt
@@ -1,8 +1,8 @@
 package com.example.androiddevchallenge.data.model
 
 data class Owner(
-    val name:String = "",
-    val photo:String = "",
-    val comment:String = ""
+    val name: String = "",
+    val photo: String = "",
+    val comment: String = ""
 
 )
